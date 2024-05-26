f5fca2ff54e00dd3cd72acc3efae9f97816f217c
André <andrebarros_@hotmail.com>
2024-05-26 11:06:41 -0300

    Utilização de props

--- Pratica/cardStar/components/cards/index.jsx
+++ Pratica/cardStar/components/cards/index.jsx
@@ -1,14 +1,13 @@
 
-import posterimg from '../../assets/star.jpg'
 import "../../styles/cards.css"
 
-export default function Cards(){
+export default function Cards(props){
     return(
         <div className='container'>
-            <img className='poster' src={posterimg} alt="" />
+            <img className='poster' src={props.img} alt="" />
             <div>
-                <h2 className='title'>Poster: Star Wards</h2>
-                <p>Texto do poster</p>
+                <h2 className='title'>{props.title}</h2>
+                <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
                 <button className='botao'>Comprar agora</button>
             </div>
         </div>
--- Pratica/cardStar/src/App.jsx
+++ Pratica/cardStar/src/App.jsx
@@ -1,9 +1,24 @@
 import Cards from '../components/cards'
+import posterimg from '../assets/star.jpg'
+import posterimg2 from '../assets/star2.jpg'
+import posterimg3 from '../assets/star3.jpg'
+
 
 export default function App(){
   return (
     <div className="app">
-      <Cards/>
+      <Cards
+       title="Pôster: Star Wars (1977)"
+       img={posterimg}           
+       />
+      <Cards
+       title="Pôster: Empire Strikes Back (1980)"
+       img={posterimg2}          
+       />
+      <Cards
+       title="Pôster: Return of the Jedi (1983)"
+       img={posterimg3}           
+        />
     </div>
   )
 } 
